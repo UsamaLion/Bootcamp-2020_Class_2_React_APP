45cddf1d1d3b47b15c0c30a311d1dff1619518dc
UsamaLion <ulion269@gmail.com>
2020-10-21 18:25:26 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 
 function App() {
   return (
-    <div>
+    <div className="App">
      <h1> Hello world</h1>
      <h2>Yes this is my first react publish online hahhaahahah</h2>
     </div>
