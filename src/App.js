f7c7db1149688fd533a4618d8d6ee72bc6986634
UsamaLion <ulion269@gmail.com>
2020-10-21 18:21:27 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import './App.css';
 function App() {
   return (
     <div>
-      Hello world
+     <h1> Hello world</h1>
+     <h2>Yes this is my first react publish online hahhaahahah</h2>
     </div>
   );
 }
